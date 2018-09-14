9ffd5a8b1f5d3c9077d26ef5a17bb6a4cf2ee417
joseminaya15 <jose.minayac15@gmail.com>
2018-09-14 10:06:54 -0500

    terminado

---
+++ application/controllers/AdminSurvey.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class AdminSurvey extends CI_Controller {
+
+	function __construct() {
+        parent::__construct();
+        $this->load->model('M_reporte');
+        $this->load->helper("url");//BORRAR CACHÉ DE LA PÁGINA
+        $this->output->set_header('Last-Modified:'.gmdate('D, d M Y H:i:s').'GMT');
+        $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate');
+        $this->output->set_header('Cache-Control: post-check=0, pre-check=0',false);
+        $this->output->set_header('Pragma: no-cache');
+    }
+	public function index(){
+		$this->load->view('v_admin_survey');
+	}
+    function cerrarCesion(){
+        $data['error'] = EXIT_ERROR;
+        $data['msj']   = null;
+        try {
+            $this->session->unset_userdata('usuario');
+            $data['error'] = EXIT_SUCCESS;
+        } catch (Exception $e){
+            $data['msj'] = $e->getMessage();
+        }
+        echo json_encode($data);
+    }
+}
